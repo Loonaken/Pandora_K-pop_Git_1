3bd1083c35e0e362014f4516b5395a37924da79a
Loonaken <kendoku328@gmail.com>
2022-12-15 16:05:37 +0900

    1-1s MVCs モデル動作確認 + foreach機能動作確認完了

--- Pandora_K-pop/resources/views/song/index.blade.php
+++ Pandora_K-pop/resources/views/song/index.blade.php
@@ -1,20 +1,24 @@
 @extends('app')
 
-@section('title', '曲選択ページ')
+@section('title')
+ランダム曲ページ||{{ config('app.name')}}
+@endsection
 
 @section('content')
 @include('nav')
 
 <div class="container mt-4">
-  <div class="card">
-    <a href="https://www.youtube.com/watch?v=SPYX2y4NzTU"><img src="/images/loona pic 3.webp" class="card-img-top"></a>
+  @foreach ($songs as $song)
+  <div class="card m-3">
+    <a href="{{$song->youtube_link}}"><img src="{{$song->mv_image}}" class="card-img-top"></a>
     <div class="card-body">
       <div class="card-title">
-        <h3>LOONA</h3>
+        <h3>{{$song->group}}</h3>
         <div class="card-subtitle">
-          <h5>Flip that</h5>
+          <h5>{{$song->title}}</h5>
         </div>
       </div>
     </div>
   </div>
+  @endforeach
 </div>
